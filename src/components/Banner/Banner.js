81bd5dc508047b4e6c53c365f7a2bedc48c8994c
deilor101 <matehenrique20@hotmail.com>
2023-12-15 14:13:52 -0300

    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export const Banner = () => {
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner da aplicação Organo" />
+        </header>
+        
+    )
+}
---
+++ src/components/Form/index.js
@@ -0,0 +1,17 @@
+import "./Form.css"
+import CampoTexto from "../CampoTexto"
+
+const Form = () => {
+    return(
+        <section className="form">
+            <form>
+            <h2>Preencha os dados para criar o card do colaborador</h2>
+            <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+            <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+            <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+            </form>
+        </section>
+    )
+}
+
+export default Form
