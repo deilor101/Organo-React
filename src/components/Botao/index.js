b44e34875d3cae0e7cc133b573803a4546b43439
deilor101 <matehenrique20@hotmail.com>
2023-12-16 16:53:29 -0300

    parte 02 finalizada

---
+++ src/components/Botao/index.js
@@ -0,0 +1,7 @@
+import "./Botao.css"
+
+export const Botao = (props) => {
+    return(
+       <button className="botao">{props.texto}</button> 
+    )
+}
---
+++ src/components/ListaSuspensa/index.js
@@ -0,0 +1,12 @@
+import "./ListaSuspensa.css"
+
+export const ListaSuspensa = (props) => {
+    return(
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select required={props.required}>
+                {props.itens.map(item => <option key={item}>{item}</option>)}
+            </select>
+        </div>
+    )
+}
